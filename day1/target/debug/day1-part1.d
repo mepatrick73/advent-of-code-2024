d99b1fa85d92e37ffa5451b967008e45b51157e7
mepatrick73 <pameu17@ulaval.ca>
2024-12-01 12:48:27 -0500

    Redoing git ignore

--- day1/target/debug/day1-part1.d
+++
@@ -1,1 +0,0 @@
-/home/patrick/personal/aoc-2025/day1/day1-part1/target/debug/day1-part1: /home/patrick/personal/aoc-2025/day1/day1-part1/src/main.rs
--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/patrick/personal/aoc-2025/day1/target/debug/day1: /home/patrick/personal/aoc-2025/day1/src/main.rs
