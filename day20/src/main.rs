0e4fdc9f139effcf58915eaa8b2cee45e787fe58
mepatrick73 <pameu17@ulaval.ca>
2024-12-28 17:17:41 -0500

    day 20 full solution

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -3,34 +3,47 @@ use std::{
     fs, io,
 };
 
-fn part_2() -> io::Result<()> {
-    let content = fs::read_to_string("res/input.txt")?;
-    let mut S = (0, 0);
-    let mut E = (0, 0);
-    let mut matrix: Vec<Vec<char>> = content.lines().map(|line| line.chars().collect()).collect();
+fn parse_matrix(content: String) -> ((usize, usize), (usize, usize), Vec<Vec<char>>, usize, usize) {
+    let mut start_coordinate = (0, 0);
+    let mut end_coordinate = (0, 0);
+    let matrix: Vec<Vec<char>> = content.lines().map(|line| line.chars().collect()).collect();
     let rows = matrix.len();
     let cols = matrix[0].len();
     for (r_idx, row) in matrix.iter().enumerate() {
         for (c_idx, char) in row.iter().enumerate() {
             if *char == 'S' {
-                S = (r_idx, c_idx);
+                start_coordinate = (r_idx, c_idx);
             }
             if *char == 'E' {
-                E = (r_idx, c_idx);
+                end_coordinate = (r_idx, c_idx);
             }
         }
     }
-    //println!("{S:?}, {E:?}");
+    (start_coordinate, end_coordinate, matrix, rows, cols)
+}
 
+fn bfs_shortest_path(
+    start_coordinate: (usize, usize),
+    end_coordinate: (usize, usize),
+    matrix: &Vec<Vec<char>>,
+    rows: usize,
+    cols: usize,
+) -> Vec<(usize, usize)> {
     let mut queue: VecDeque<(usize, usize, usize)> = VecDeque::new();
     let mut visited: HashSet<(usize, usize)> = HashSet::new();
-    visited.insert(S);
-    queue.push_front((S.0, S.1, 0));
-    let mut normal_time = 0;
+    let mut pred: HashMap<(usize, usize), (usize, usize)> = HashMap::new();
+    visited.insert(start_coordinate);
+    queue.push_front((start_coordinate.0, start_coordinate.1, 0));
+    let mut shortest_path: Vec<(usize, usize)> = vec![];
     while !queue.is_empty() {
         let (row, col, lenght) = queue.pop_back().unwrap();
-        if (row, col) == E {
-            normal_time = lenght;
+        if (row, col) == end_coordinate {
+            let mut curr = Some(&end_coordinate);
+            while let Some(curr_node) = curr {
+                shortest_path.push(*curr_node);
+                curr = pred.get(&curr_node);
+            }
+            shortest_path.reverse();
             break;
         }
         let ns = [(-1, 0), (1, 0), (0, -1), (0, 1)];
@@ -45,182 +58,59 @@ fn part_2() -> io::Result<()> {
             {
                 queue.push_front((n_row as usize, n_col as usize, lenght + 1));
                 visited.insert((n_row as usize, n_col as usize));
+                pred.insert((n_row as usize, n_col as usize), (row, col));
             }
         }
     }
-    let mut single_cheats: HashSet<(usize, usize)> = HashSet::new();
-    let mut double_cheats: HashSet<((usize, usize), (usize, usize))> = HashSet::new();
+    return shortest_path;
+}
 
-    //println!("{normal_time}");
-    for row in 0..matrix.len() {
-        for col in 0..matrix[0].len() {
-            if matrix[row][col] == '#' {
-                single_cheats.insert((row, col));
-            }
-        }
-    }
-    for row in 0..matrix.len() {
-        for col in 0..matrix[0].len() - 1 {
-            if matrix[row][col] == '#' && matrix[row][col + 1] == '#' {
-                double_cheats.insert(((row, col), (row, col + 1)));
-            }
-        }
-    }
-    let mut freq = HashMap::new();
-    let single_cheat_len = single_cheats.len();
-    for (index, (wall_row, wall_col)) in single_cheats.into_iter().enumerate() {
-        matrix[wall_row][wall_col] = '.';
-        let mut queue: VecDeque<(usize, usize, usize)> = VecDeque::new();
-        let mut pred: HashMap<(usize, usize), (usize, usize)> = HashMap::new();
-        let mut visited: HashSet<(usize, usize)> = HashSet::new();
-        visited.insert(S);
-        queue.push_front((S.0, S.1, 0));
-        let mut new_time = 0;
-        while !queue.is_empty() {
-            let (row, col, lenght) = queue.pop_back().unwrap();
-            if (row, col) == E {
-                new_time = lenght;
-                break;
-            }
-            let ns = [(-1, 0), (1, 0), (0, -1), (0, 1)];
-            for (nx, ny) in ns {
-                let n_row = row as i32 + nx;
-                let n_col = col as i32 + ny;
-                if n_row < 0 || n_col < 0 || n_row >= rows as i32 || n_col >= cols as i32 {
-                    continue;
-                }
-                if !visited.contains(&(n_row as usize, n_col as usize))
-                    && matrix[n_row as usize][n_col as usize] != '#'
-                {
-                    pred.insert((n_row as usize, n_col as usize), (row, col));
-                    queue.push_front((n_row as usize, n_col as usize, lenght + 1));
-                    visited.insert((n_row as usize, n_col as usize));
+fn cheat_distance(
+    start_coordinate: (usize, usize),
+    end_coordinate: (usize, usize),
+    rows: usize,
+    cols: usize,
+    matrix: Vec<Vec<char>>,
+    cheat_length: i32,
+) -> usize {
+    let shortest_path = bfs_shortest_path(start_coordinate, end_coordinate, &matrix, rows, cols);
+    let normal_time = shortest_path.len() - 1;
+    let mut freq: HashMap<usize, usize> = HashMap::new();
+    for i in 0..shortest_path.len() {
+        let node_1 = shortest_path[i];
+        for j in i + 1..shortest_path.len() {
+            let node_2 = shortest_path[j];
+            let man_distance = (node_1.0 as i32 - node_2.0 as i32).abs() as i32
+                + (node_1.1 as i32 - node_2.1 as i32).abs() as i32;
+            if man_distance <= cheat_length {
+                let total_distance = i + man_distance as usize + shortest_path.len() - j - 1;
+                if total_distance < normal_time && normal_time - total_distance >= 100 {
+                    freq.entry(normal_time - total_distance)
+                        .and_modify(|e| *e += 1)
+                        .or_insert(1);
                 }
             }
         }
-        matrix[wall_row][wall_col] = '#';
-        *freq.entry(normal_time - new_time).or_insert(0) += 1;
-        println!(
-            "{}% done in single cheats",
-            index as f32 / single_cheat_len as f32
-        );
     }
-    let res = freq
-        .clone()
-        .into_iter()
-        .filter(|(key, _value)| *key >= 100)
-        .fold(0, |acc, (_key, value)| acc + value);
-    println!("day 18 part 1 result is : {:?}", res);
+    let res = freq.into_iter().fold(0, |acc, (_key, value)| acc + value);
+
+    res
+}
+
+fn part_2() -> io::Result<()> {
+    let content = fs::read_to_string("res/input.txt")?;
+    let (start_coordinate, end_coordinate, matrix, rows, cols) = parse_matrix(content);
+    let res = cheat_distance(start_coordinate, end_coordinate, rows, cols, matrix, 20);
+    println!("Day 20 part 2 result is : {res}");
     Ok(())
 }
 
 fn part_1() -> io::Result<()> {
     let content = fs::read_to_string("res/input.txt")?;
-    let mut S = (0, 0);
-    let mut E = (0, 0);
-    let mut matrix: Vec<Vec<char>> = content.lines().map(|line| line.chars().collect()).collect();
-    let rows = matrix.len();
-    let cols = matrix[0].len();
-    for (r_idx, row) in matrix.iter().enumerate() {
-        for (c_idx, char) in row.iter().enumerate() {
-            if *char == 'S' {
-                S = (r_idx, c_idx);
-            }
-            if *char == 'E' {
-                E = (r_idx, c_idx);
-            }
-        }
-    }
-    //println!("{S:?}, {E:?}");
-
-    let mut queue: VecDeque<(usize, usize, usize)> = VecDeque::new();
-    let mut visited: HashSet<(usize, usize)> = HashSet::new();
-    visited.insert(S);
-    queue.push_front((S.0, S.1, 0));
-    let mut normal_time = 0;
-    while !queue.is_empty() {
-        let (row, col, lenght) = queue.pop_back().unwrap();
-        if (row, col) == E {
-            normal_time = lenght;
-            break;
-        }
-        let ns = [(-1, 0), (1, 0), (0, -1), (0, 1)];
-        for (nx, ny) in ns {
-            let n_row = row as i32 + nx;
-            let n_col = col as i32 + ny;
-            if n_row < 0 || n_col < 0 || n_row >= rows as i32 || n_col >= cols as i32 {
-                continue;
-            }
-            if !visited.contains(&(n_row as usize, n_col as usize))
-                && matrix[n_row as usize][n_col as usize] != '#'
-            {
-                queue.push_front((n_row as usize, n_col as usize, lenght + 1));
-                visited.insert((n_row as usize, n_col as usize));
-            }
-        }
-    }
-    let mut single_cheats: HashSet<(usize, usize)> = HashSet::new();
-
-    //println!("{normal_time}");
-    for row in 0..matrix.len() {
-        for col in 0..matrix[0].len() {
-            if matrix[row][col] == '#' {
-                single_cheats.insert((row, col));
-            }
-        }
-    }
-    let mut freq = HashMap::new();
-    let single_cheat_len = single_cheats.len();
-    for (index, (wall_row, wall_col)) in single_cheats.into_iter().enumerate() {
-        matrix[wall_row][wall_col] = '.';
-        let mut queue: VecDeque<(usize, usize, usize)> = VecDeque::new();
-        let mut pred: HashMap<(usize, usize), (usize, usize)> = HashMap::new();
-        let mut visited: HashSet<(usize, usize)> = HashSet::new();
-        visited.insert(S);
-        queue.push_front((S.0, S.1, 0));
-        let mut new_time = 0;
-        while !queue.is_empty() {
-            let (row, col, lenght) = queue.pop_back().unwrap();
-            if (row, col) == E {
-                new_time = lenght;
-                break;
-            }
-            let ns = [(-1, 0), (1, 0), (0, -1), (0, 1)];
-            for (nx, ny) in ns {
-                let n_row = row as i32 + nx;
-                let n_col = col as i32 + ny;
-                if n_row < 0 || n_col < 0 || n_row >= rows as i32 || n_col >= cols as i32 {
-                    continue;
-                }
-                if !visited.contains(&(n_row as usize, n_col as usize))
-                    && matrix[n_row as usize][n_col as usize] != '#'
-                {
-                    pred.insert((n_row as usize, n_col as usize), (row, col));
-                    queue.push_front((n_row as usize, n_col as usize, lenght + 1));
-                    visited.insert((n_row as usize, n_col as usize));
-                }
-            }
-        }
-        matrix[wall_row][wall_col] = '#';
-        *freq.entry(normal_time - new_time).or_insert(0) += 1;
-        println!(
-            "{}% done in single cheats",
-            index as f32 / single_cheat_len as f32
-        );
-    }
-    let res = freq
-        .clone()
-        .into_iter()
-        .filter(|(key, _value)| *key >= 100)
-        .fold(0, |acc, (_key, value)| acc + value);
-    println!("day 18 part 1 result is : {:?}", res);
+    let (start_coordinate, end_coordinate, matrix, rows, cols) = parse_matrix(content);
+    let res = cheat_distance(start_coordinate, end_coordinate, rows, cols, matrix, 2);
+    println!("Day 20 part 1 result is : {res}");
     Ok(())
-}
-
-fn is_sub<T: PartialEq>(haystack: &[T], needle: &[T]) -> bool {
-    haystack
-        .windows(needle.len())
-        .any(|window| window == needle)
 }
 
 fn main() -> io::Result<()> {
